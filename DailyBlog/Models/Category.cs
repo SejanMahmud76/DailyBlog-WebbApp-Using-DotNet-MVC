6b24a9a8b25ead99a6bcddb5a716ee0463241459
Rakibul-Hasan-Sezan <sejan16@cse.pstu.ac.bd>
2025-07-13 14:45:01 +0600

    i have created another model for  category called category.cs and defined its properties.

---
+++ DailyBlog/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace DailyBlog.Models
+{
+    public class Category
+    {
+
+        public int Id { get; set; }
+
+        public string Nmae { get; set; }
+
+
+        public string des { get; set; }
+
+
+    }
+}
