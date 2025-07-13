f11865fbac7ef1894e2fb8d10e74a357ce4ec511
Rakibul-Hasan-Sezan <sejan16@cse.pstu.ac.bd>
2025-07-13 11:28:06 +0600

    Add project files.

---
+++ DailyBlog/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿namespace DailyBlog.Models
+{
+    public class Post
+    {
+        // declaring the features  of post method
+         public int Id { get; set; }
+         
+         public String Title { get; set; }
+
+         public string Content { get; set; }
+
+         public string Author { get; set; }
+
+        public string FeatureImagePath { get; set; }
+
+        public  DateTime PublishedDate { get; set; }
+
+
+
+
+
+    }
+}
