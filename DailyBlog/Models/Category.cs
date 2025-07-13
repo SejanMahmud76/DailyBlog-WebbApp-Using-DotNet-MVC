7cd3e7857349df72815b536e63262f1213e9329c
Rakibul-Hasan-Sezan <sejan16@cse.pstu.ac.bd>
2025-07-13 14:54:04 +0600

    i have declared attributes for the properties of the category,cs

--- DailyBlog/Models/Category.cs
+++ DailyBlog/Models/Category.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.AspNetCore.Routing.Constraints;
+using System.ComponentModel.DataAnnotations;
 
 namespace DailyBlog.Models
 {
     public class Category
     {
 
+        [Key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage ="The Category  Nmae is required")]
+        [MaxLength(100, ErrorMessage =" The category name can not exceed 100 word")]
         public string Nmae { get; set; }
 
 
-        public string des { get; set; }
+        public string? des { get; set; }// with "?" i dwclare it as nullable member, meaning it can be null
+                                        // for this nullable should be enabbled in the project file
 
 
     }
