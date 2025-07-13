ffcf4a0a5b4df44fbc8325f67f57742a73503a92
Rakibul-Hasan-Sezan <sejan16@cse.pstu.ac.bd>
2025-07-13 15:09:17 +0600

    Created a new model for the comment called commemnt.cs, and Declared  its properties and Attributes

---
+++ DailyBlog/Models/Comment.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DailyBlog.Models
+{
+    public class Comment
+    {
+
+
+        [Key] 
+        public int Id { get; set; }
+
+
+
+        [Required(ErrorMessage = "The   UserNmae is required")]
+        [MaxLength(100, ErrorMessage = " The UserName can not exceed 100 word")]
+
+        public string UserName { get; set; }
+
+
+
+        [DataType(DataType.Date)]
+        public DateTime CommentDate { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
