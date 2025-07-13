4eb34239e13387b06731484e7d2000a283f67045
Rakibul-Hasan-Sezan <sejan16@cse.pstu.ac.bd>
2025-07-13 12:49:40 +0600

    In this stage i declared some Attributes for  the properties of the post.cs

--- DailyBlog/Models/Post.cs
+++ DailyBlog/Models/Post.cs
@@ -1,19 +1,44 @@
-﻿namespace DailyBlog.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DailyBlog.Models
 {
     public class Post
     {
-        // declaring the features  of post method
-         public int Id { get; set; }
-         
+        //step1: declaring the properties  of post method
+        //step2: Now we will add some attributes to these properties
+
+        [Key]
+        public int Id { get; set; }
+
+
+
+        [Required(ErrorMessage = "The Title is Required")]
+        [MaxLength (400, ErrorMessage = "The title can not exceed 400 character")]
          public String Title { get; set; }
 
-         public string Content { get; set; }
 
-         public string Author { get; set; }
+
+
+        [Required(ErrorMessage = "The Content is Required")]
+        public string Content { get; set; }
+
+
+
+
+        [Required(ErrorMessage = "The Author is Required")]
+        [MaxLength(100, ErrorMessage = "Author name cannot exceed 100 character")]
+        public string Author { get; set; }
+
+
 
         public string FeatureImagePath { get; set; }
 
-        public  DateTime PublishedDate { get; set; }
+
+
+        [DataType(DataType.Date)] //only for showing Date not Time
+
+        public  DateTime PublishedDate { get; set; } = DateTime.
+            Now;
 
 
 
